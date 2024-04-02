c13f7fa018c343589cf892f190fa640639267804
Obala <oballasteve4@gmail.com>
2024-04-02 12:37:13 +0300

    adds arrays

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution here!
+let cats=["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+  cats.push("Ralph");
+  cats.unshift();
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name);}
+    function destructivelyRemoveLastCat() {
+       return cats.pop();}
+        function destructivelyRemoveFirstCat() {
+           return cats.shift();}
+            function appendCat(name) {
+                return cats.concat(name);
+              }
+              function prependCat(name) {
+
+                return [name, ...cats];}
+                function removeLastCat() {
+                    return cats.slice(0,cats.length -1);}
+                    function removeFirstCat() {
+                        return cats.slice(1);}
